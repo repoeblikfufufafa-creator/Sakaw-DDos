086e14be9b51d63c859fe434110f785b10e47135
repoeblikfufufafa-creator <repoeblikfufufafa@gmail.com>
2026-01-07 15:25:49 +0700

    Update termulkobam.py

--- termulkobam.py
+++ termulkobam.py
@@ -105,7 +105,7 @@ class HTTPThread(threading.Thread):
 				code=httpcall(url)
 				if (code==500) & (safe==1):
 					set_flag(2)
-		except Exception, ex:
+		except (Exception, ex):
 			pass
 
 # monitors http threads and counts requests

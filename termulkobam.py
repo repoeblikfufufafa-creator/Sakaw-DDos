8e295897ae9ff8a22d349d0d5a822e64e158e136
repoeblikfufufafa-creator <repoeblikfufufafa@gmail.com>
2026-01-07 16:21:00 +0700

    Update termulkobam.py

--- termulkobam.py
+++ termulkobam.py
@@ -88,9 +88,7 @@ def httpcall(url):
 	except (urllib2.HTTPError, e):
 			#print e.code
 			set_flag(1)
-		    print("""
-			print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
-			""")
+			print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 			code=500
 	except urllib2.URLError, e:
 			#print e.reason

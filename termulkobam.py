d1a3307b3167911dba3af78c60cf0fabaffaf3a3
repoeblikfufufafa-creator <repoeblikfufufafa@gmail.com>
2026-01-07 15:24:32 +0700

    Update termulkobam.py

--- termulkobam.py
+++ termulkobam.py
@@ -86,7 +86,7 @@ def httpcall(url):
 	except (urllib2.HTTPError, e):
 			#print e.code
 			set_flag(1)
-			print 'Response Code 500'
+			print('Response Code 500')
 			code=500
 	except (urllib2.URLError, e):
 			#print e.reason
